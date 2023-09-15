18b9029ad0a74c3d5e326603d7ff54d1f1c474f5
barathbarath23 <barathkesavan2310@gmail.com>
2023-09-15 00:55:32 -0800

    Swap

---
+++ SwappingProgram.java
@@ -0,0 +1,22 @@
+package first;
+
+public class SwappingProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=2;
+		int j=3;
+		int k=4;
+		
+		i=j;
+		j=k;
+		k=i;
+		System.out.print(i);
+		System.out.print(j);
+		System.out.print(k);
+	}
+	
+	
+
+}
